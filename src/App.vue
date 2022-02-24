6889bbfdf6a128ba2a903966d7945f67333ee52d
AnAlgorithm <64064699+AnAlgorithm@users.noreply.github.com>
2022-02-24 16:48:38 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,15 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: #2c3e50;
   color: #eceff4;
   margin-top: 60px;
 }
